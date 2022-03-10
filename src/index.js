45b87875166f0325f4507ff7624e16e2fedae860
4lexLopez <alex.roman.lopez@gmail.com>
2022-03-09 22:24:59 -0500

    HTML 5 CSS3 JS exercise finished

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
-window.onload = onLoad
+window.onload = function() {
+  addAccordionListener();
+  addThemeListeners();
+};
 
-function onLoad() {
-  var acc = document.getElementsByClassName("accordion");
-  var i;
+function addAccordionListener() {
+  let acc = document.getElementsByClassName('accordion');
 
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
+  for (let i of acc) {
+    i.addEventListener('click', function() {
+      this.classList.toggle('active');
       var panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
+      if (panel.style.display === 'block') {
+        panel.style.display = 'none';
       } else {
-        panel.style.display = "block";
+        panel.style.display = 'block';
       }
     });
+  }}
+
+function addThemeListeners() {
+
+let switches = document.getElementsByClassName('switch');
+let style = localStorage.getItem('style');
+
+  if (style == null) {
+    setTheme('sky');
+  } else {
+    setTheme(style);
   }
+
+  for (let i of switches) {
+    i.addEventListener('click', function () {
+      let theme = this.dataset.theme;
+      setTheme(theme);
+    });
+  }
+}
+
+function setTheme(theme) {
+  if (theme == 'light') {
+    document.getElementById('switcher-id').href = '/css/light.css';
+  } else if (theme == 'sky') {
+    document.getElementById('switcher-id').href = '/css/sky.css';
+  } else if (theme == 'purple') {
+    document.getElementById('switcher-id').href = '/css/purple.css';
+  }
+  localStorage.setItem('style', theme);
 }
